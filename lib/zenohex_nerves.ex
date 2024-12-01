0a7f07e6669e4457ae497a186aa31ad03701e3a9
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2024-12-01 18:05:05 +0900

    mix nerves.new zenohex_nerves

---
+++ lib/zenohex_nerves.ex
@@ -0,0 +1,18 @@
+defmodule ZenohexNerves do
+  @moduledoc """
+  Documentation for `ZenohexNerves`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ZenohexNerves.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/zenohex_nerves_test.exs
@@ -0,0 +1,8 @@
+defmodule ZenohexNervesTest do
+  use ExUnit.Case
+  doctest ZenohexNerves
+
+  test "greets the world" do
+    assert ZenohexNerves.hello() == :world
+  end
+end
